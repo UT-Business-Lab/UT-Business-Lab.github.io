2919990a56d8a93abe5ac5a6356f20958fe1c207
r.shigaki <ryusei_shigaki@en-konkatsu.com>
2019-12-11 16:14:26 +0900

    各設定ファイル追加

--- javascript/swiper_script.js
+++ javascript/swiper_script.js
@@ -4,14 +4,14 @@ var mySwiper = new Swiper ('.swiper-container', {
     speed: 600, // スライドが切り替わるトランジション時間(ミリ秒)。
     slidesPerView: 4, // 何枚のスライドを表示するか
     spaceBetween: 10, // スライド間の余白サイズ(ピクセル)
-    direction: 'horizontal', // スライド方向。 'horizontal'(水平) か 'vertical'(垂直)。effectオプションが 'slide' 以外は無効。
+    direction: 'vertical', // スライド方向。 'horizontal'(水平) か 'vertical'(垂直)。effectオプションが 'slide' 以外は無効。
     effect: 'slide', // "slide", "fade"(フェード), "cube"(キューブ回転), "coverflow"(カバーフロー) または "flip"(平面回転)
 
     // スライダーの自動再生
     // autoplay: true 　のみなら既定値での自動再生
     autoplay: {
-        delay: 3000, // スライドが切り替わるまでの表示時間(ミリ秒)
-        stopOnLast: false, // 最後のスライドまで表示されたら自動再生を中止するか
+        delay: 700, // スライドが切り替わるまでの表示時間(ミリ秒)
+        stopOnLast: true, // 最後のスライドまで表示されたら自動再生を中止するか
         disableOnInteraction: true // ユーザーのスワイプ操作を検出したら自動再生を中止するか
     },
 
